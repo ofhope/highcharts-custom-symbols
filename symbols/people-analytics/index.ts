bf37794fe6fbe664fc6fa0393fcc9f533e891894
Alexis Hope <alexis@cvofhope.com>
2026-04-10 13:52:58 +1000

    chore: delete old symbols

--- symbols/people-analytics/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './baby';
-export * from './building-apartment';
-export * from './building-office';
-export * from './building';
-export * from './cross';
-export * from './gift';
-export * from './house';
